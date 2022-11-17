cc2d0626100cc8f747f7f4efb1b827a3361b3495
AsheThePixel <ashe.ian.walker@Gmail.com>
2022-11-17 08:47:20 +0200

    Walker_Ashe_SoftwareDev_Term4_Assignment

--- js/validate.js
+++ js/validate.js
@@ -1,21 +1,28 @@
 function newTask(){
 
     // Dom api references
-
     // 1. get the newTask element/inpu by its own id
-    // 2. create an if statement to prevent HTML Characters
+    var newTaskInput = document.getElementById("newTask").value;
 
-    if(true){
+    var task = document.getElementById("task");
+    
+    // 2. create an if statement to prevent HTML Characters
+    if(newTaskInput.includes('>' || '<')){
         document.getElementById("error").style.display ="Block";
     }
 
     // 3. create a new li element using createElement dom api
+    var li = document.createElement('li');
+
     // 4. create a textnode (save this to a variable)
-    // 5. append this variable to the <li> variable by using .appendChild
-    // 6. Append your new list to the HTML unordered list
+    var textNode = document.createTextNode("Have a Wonderful Day");
     
+    // 5. append this variable to the <li> variable by using .appendChild
+    li.appendChild(textNode);
+
+    // 6. Append your new list to the HTML unordered list
+    task.appendChild(li);
 }
 
 //Calling the function
-
 newTask();
